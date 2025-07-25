0fc0d7f49996661258db9fe15a0aaa4bbdd2949e
ThunderRabbit <zuzazibubabi@gmail.com>
2025-07-26 06:31:14 +0800

    trying to change the order line of new quotations

---
+++ custom/sale_batch_no/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale: Batch No on Quotation",
+    "version": "18.0.1.0.0",
+    "summary": "Adds a batch number field to sales order lines",
+    "category": "Sales",
+    "author": "You",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
